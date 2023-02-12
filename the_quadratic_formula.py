2deffedc53391343f589343c1add06b2f23c015f
Uchenna Adubasim <uchadubs@yahoo.com>
2023-02-12 21:15:14 +0800

    Refactoring

--- the_quadratic_formula.py
+++ the_quadratic_formula.py
@@ -10,7 +10,6 @@ def quadratic_formula():
 	"""Model the quadratic formula"""
 	while True:
 		show_messages()
-
 		a = input('Enter a : ')
 		if a == 'q' or a == 'Q':
 			break
@@ -25,31 +24,23 @@ def quadratic_formula():
 
 		#Handle errors from user input and continue executing program
 		try:
-			a = float(a)
-			b = float(b)
-			c = float(c)
+			a, b, c = float(a), float(b), float(c)
 	
-			# Calculate vertex and axis of symmetry
-			v = the_vertex(a, b, c)
+			# Calculate vertex and discriminant
+			v, d = the_vertex(a, b, c), discriminant(a, b, c)
 
 			#if the discriminant is non-negative, it will have real roots and 
 				# will require python's math library
-			d = discriminant(a, b, c)
-
 			if d >= 0:
-				x_1 = (-b + math.sqrt(d))/(2*a)
-				x_2 = (-b - math.sqrt(d))/(2*a)
-
+				x_1, x_2 = (-b + math.sqrt(d))/(2*a), (-b - math.sqrt(d))/(2*a)
 
 			#if discriminant is negative, it will have complex roots and will 
 				# require python's complex math library
 			else:
-				x_1 = (-b + cmath.sqrt(d))/(2*a)
-				x_2 = (-b - cmath.sqrt(d))/(2*a)
+				x_1, x_2 = (-b + cmath.sqrt(d))/(2*a), (-b - cmath.sqrt(d))/(2*a)
 
-			print(f"\nThe roots are {x_1} and {x_2}")
-			print(f"\nDiscriminant: {d}\nVertex: {v}\n" 
-					f"Axis of Symmetry: x = {v[0]}\n")
+			print(f"\nThe roots are {x_1} and {x_2}\nDiscriminant: "
+				f"{d}\nVertex: {v}\nAxis of Symmetry: x = {v[0]}\n")
 
 		except (ValueError, ArithmeticError):
 			print("\nNot a quadratic equation!\n")
