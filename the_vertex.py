f84fe195a705097163d6e7a07698f40a473b5637
Uchenna Adubasim <uchadubs@yahoo.com>
2023-02-24 21:18:41 +0800

    Refactored oop

--- the_vertex.py
+++
@@ -1,9 +0,0 @@
-# Vertex of a quadratic function is at x = -b/2a
-
-def vertex(a, b, c):
-	"""Return Tuple containing the vertex of the quadratic function"""
-	x = (-b) / (2*a)
-	y = a*(x**2) + (b*x) + c
-
-	ordered_pair = (x, y)
-	return ordered_pair
