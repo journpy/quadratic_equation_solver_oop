61449bda441460f2f1280683420d1c54e1a715da
Uchenna Adubasim <uchadubs@yahoo.com>
2023-10-26 09:26:00 +0100

    ready to push to github

--- quadratic_functions.py
+++ quadratic_functions.py
@@ -47,12 +47,14 @@ class QuadraticFunctions:
 		# If the discriminant is non-negative, it will have real roots and 
 			# will require python's math library.
 		if d >= 0:
-			x_1, x_2 = (-self.b + math.sqrt(d))/(2*self.a), (-self.b - math.sqrt(d))/(2*self.a)
+			x_1 = (-self.b + math.sqrt(d))/(2*self.a)
+			x_2 = (-self.b - math.sqrt(d))/(2*self.a)
 
 		# If discriminant is negative, it will have complex roots and will 
 			# require python's complex math library.
 		else:
-			x_1, x_2 = (-self.b + cmath.sqrt(d))/(2*self.a), (-self.b - cmath.sqrt(d))/(2*self.a)
+			x_1 = (-self.b + cmath.sqrt(d))/(2*self.a)
+			x_2 = (-self.b - cmath.sqrt(d))/(2*self.a)
 
 		print(f"\nThe roots are {x_1} and {x_2}\nDiscriminant: "
 			f"{d}\nVertex: {v}\nAxis of Symmetry: x = {v[0]}\n")
@@ -64,16 +66,25 @@ class QuadraticFunctions:
 		plt.xlabel('x', loc='right', c='r')
 		plt.ylabel('y', loc='top', c='r')
 		plt.grid(True, linestyle=':')
+		plt.title(f"Graph of ${self.a}x^2 + {self.b}x + {self.c}$", 
+			fontsize=16, c='r')
 
 
 	def plot_and_display_graph(self):
 		"""Plot and display graph of the quadratic function."""
-		x = np.linspace(-20, 20, 50)
+		x = np.linspace(-10, 10, 50)
 		y = (self.a*(x)**2) + (self.b*x) + self.c
+
 		qf = QuadraticFunctions(self.a, self.b, self.c)
 		qf.style_graph()
-		plt.title(f"Graph of {self.a}x^2 + {self.b}x + {self.c}.", fontsize=12, fontweight='bold', c='r')
-		plt.plot(x, y, 'b')
+
+		show_roots = np.roots([self.a, self.b, self.c])
+		show_x_values = [show_roots[0], show_roots[1]]
+		show_y_values = [0, 0]
+		plt.scatter(show_x_values, show_y_values, s=100, c='r', 
+				alpha=1, label='roots')
+		plt.legend()
+		plt.plot(x, y, 'b', linewidth=3)
 		plt.show()
 
 
